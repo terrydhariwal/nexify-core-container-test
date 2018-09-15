69a5ec481c8e3277f6f8c0102801a5ae797b070d
Terry Dhariwal <terry.dhariwal@me.com>
2018-09-15 14:04:24 +0100

    set JVM size to 1024

--- lens2northwind/setenv.sh
+++ lens2northwind/setenv.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-export CATALINA_OPTS="$CATALINA_OPTS -Xms12288m -Xmx12288m"
+#export CATALINA_OPTS="$CATALINA_OPTS -Xms12288m -Xmx12288m"
+export CATALINA_OPTS="$CATALINA_OPTS -Xms1024m -Xmx1024m"
 
 export CLASSPATH="$CLASSPATH:$CATALINA_HOME/lib/*:"`/opt/hbase/bin/hbase classpath`
