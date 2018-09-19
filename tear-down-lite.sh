70f4ae21dd46fa234c7fcab868b510f4bd5491a4
Terry Dhariwal <terry.dhariwal@me.com>
2018-09-20 00:58:56 +0100

    fix bugs with tear-down.sh

--- tear-down-lite.sh
+++ tear-down-lite.sh
@@ -5,10 +5,7 @@ gcloud config set project ${PROJECT_ID};
 gcloud config set compute/zone ${COMPUTE_ZONE};
 
 export K8_CLUSTER_NAME=test-cluster-2
-
-export cluster_names=`gcloud container clusters list --zone=${COMPUTE_ZONE} --format=json | jq -r ".[].name"`
 gcloud container clusters get-credentials ${K8_CLUSTER_NAME} --zone "${COMPUTE_ZONE}"
-
 
 POD_IP=`kubectl describe pod ${deployment_name} | grep IP` #set as variable
 echo ${POD_IP}
@@ -34,7 +31,8 @@ NEW_SOURCE_RANGE=`echo $NEW_SOURCE_RANGE | sed -e 's/\]//g'`
 NEW_SOURCE_RANGE=`echo $NEW_SOURCE_RANGE | sed -e 's/\]//g'`
 gcloud compute firewall-rules update hortonworks-network-ambari-access --source-ranges="${NEW_SOURCE_RANGE}"
 
-SERVICE_LOAD_BALANCER=`kubectl get service ${deployment_name} -o=json` #set as variable
+#SERVICE_LOAD_BALANCER=`kubectl get service ${deployment_name} -o=json` #set as variable
+SERVICE_LOAD_BALANCER=`kubectl get service -o=json` #set as variable
 echo ${SERVICE_LOAD_BALANCER} | jq
 SERVICE_LOAD_BALANCER=`echo ${SERVICE_LOAD_BALANCER} | jq -r ".status.loadBalancer.ingress[0].ip"`
 echo ${SERVICE_LOAD_BALANCER}
