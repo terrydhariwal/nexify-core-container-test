6477dddbece7c39331ab6a17f25b283310cfffb8
Terry Dhariwal <terry.dhariwal@me.com>
2018-09-20 00:27:50 +0100

    clean up code

--- test-container.sh
+++ test-container.sh
@@ -11,18 +11,18 @@ export deployment_name=${deployment_app}
 gcloud container clusters get-credentials ${K8_CLUSTER_NAME} --zone "${COMPUTE_ZONE}"
 
 # get IP address of service load balancer
-SERVICE_LOAD_BALANCER=`kubectl get service ${deployment_name} -o=json` #set as variable
+#SERVICE_LOAD_BALANCER=`kubectl get service ${deployment_name} -o=json` #set as variable
+SERVICE_LOAD_BALANCER=`kubectl get service -o=json` #get all load balacners
 echo ${SERVICE_LOAD_BALANCER} | jq
-#NEED TO HANDLE ARRAY IF IPs here
-#SERVICE_LOAD_BALANCER=`echo ${SERVICE_LOAD_BALANCER} | jq -r ".status.loadBalancer.ingress[0].ip"`
-#echo ${SERVICE_LOAD_BALANCER} | jq --arg deployment_name "$deployment_name" '.items[] '
-#echo ${SERVICE_LOAD_BALANCER} | jq --arg deployment_name "$deployment_name" '.items[] | select(.metadata.name == "quorum360")'
-#echo ${SERVICE_LOAD_BALANCER} | jq --arg deployment_name "$deployment_name" '.items[] | select(.metadata.name == $deployment_name)'
-#echo ${SERVICE_LOAD_BALANCER} | jq --arg deployment_name "$deployment_name" '.items[] | select(.metadata.name == $deployment_name) | .status.loadBalancer.ingress[].ip'
+#NEED TO HANDLE ARRAY OF IPs here
+# from the various list of load balancers, find the one matchuing our deployment_name, and get its IP
 SERVICE_LOAD_BALANCER=`echo ${SERVICE_LOAD_BALANCER} | jq --arg deployment_name "$deployment_name" '.items[]  | select(.metadata.name == $deployment_name)| .status.loadBalancer.ingress[].ip'`
 echo ${SERVICE_LOAD_BALANCER}
+
+#Strip off ""
 SERVICE_LOAD_BALANCER=`echo $SERVICE_LOAD_BALANCER | sed -e 's/"//g'`
 echo ${SERVICE_LOAD_BALANCER}
+
 CONTAINER_PORT=8080
 
 # TEST TOMCAT HOMEPAGE
