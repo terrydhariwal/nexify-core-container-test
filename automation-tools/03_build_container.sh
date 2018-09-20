1dc9ea8c28a38a105fc62bf8baa5c951054962b4
Terry Dhariwal <terry.dhariwal@me.com>
2018-09-20 23:52:47 +0100

    test automation tools 1 to 3

--- automation-tools/03_build_container.sh
+++ automation-tools/03_build_container.sh
@@ -1,5 +1,8 @@
-CURRENT_DIR=$(dirname $0)
+#CURRENT_DIR=$(dirname $0)
+CURRENT_DIR=`pwd`
+echo ${CURRENT_DIR}
 cd ${CURRENT_DIR}/../lens2northwind/
+pwd
 docker build -t ${image_name} .
 cd ${CURRENT_DIR}
 docker tag ${image_name} gcr.io/${PROJECT_ID}/quorum360:${image_name}
