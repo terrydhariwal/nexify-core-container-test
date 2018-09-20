bc6cd88d02d4323f1f1e7a62082f7876d920be68
Terry Dhariwal <terry.dhariwal@me.com>
2018-09-20 23:22:18 +0100

    test automation tools 1 to 3

--- automation-tools/03_build_container.sh
+++ automation-tools/03_build_container.sh
@@ -1,6 +1,6 @@
 CURRENT_DIR=$(dirname $0)
 cd ${CURRENT_DIR}/../lens2northwind/
-docker build -t quorum360-halyard-v1.5-4gbram .
-cd CURRENT_DIR
+docker build -t ${image_name} .
+cd ${CURRENT_DIR}
 docker tag ${image_name} gcr.io/${PROJECT_ID}/quorum360:${image_name}
 docker push gcr.io/${PROJECT_ID}/quorum360:${image_name}
