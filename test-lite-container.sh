1c1c7f59f8ba36cdecdbf5c098d3cdd897674378
Terry Dhariwal <terry.dhariwal@me.com>
2018-09-20 01:40:36 +0100

    removed debug messages

--- test-lite-container.sh
+++ test-lite-container.sh
@@ -13,15 +13,15 @@ gcloud container clusters get-credentials ${K8_CLUSTER_NAME} --zone "${COMPUTE_Z
 # get IP address of service load balancer
 #SERVICE_LOAD_BALANCER=`kubectl get service ${deployment_name} -o=json` #set as variable
 SERVICE_LOAD_BALANCER=`kubectl get service -o=json` #get all load balacners
-echo ${SERVICE_LOAD_BALANCER} | jq
+#echo ${SERVICE_LOAD_BALANCER} | jq
 #NEED TO HANDLE ARRAY OF IPs here
 # from the various list of load balancers, find the one matchuing our deployment_name, and get its IP
 SERVICE_LOAD_BALANCER=`echo ${SERVICE_LOAD_BALANCER} | jq --arg deployment_name "$deployment_name" '.items[]  | select(.metadata.name == $deployment_name)| .status.loadBalancer.ingress[].ip'`
-echo ${SERVICE_LOAD_BALANCER}
+#echo ${SERVICE_LOAD_BALANCER}
 
 #Strip off ""
 SERVICE_LOAD_BALANCER=`echo $SERVICE_LOAD_BALANCER | sed -e 's/"//g'`
-echo ${SERVICE_LOAD_BALANCER}
+#echo ${SERVICE_LOAD_BALANCER}
 
 CONTAINER_PORT=8080
 
