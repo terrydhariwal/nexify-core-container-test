94b89cd35d1b70a2ad79becdf94f12d55c013161
Terry Dhariwal <terry.dhariwal@me.com>
2018-09-21 13:02:22 +0100

    test automation tools 1 to 4

--- automation-tools/set_cluster_node_spec.py
+++ automation-tools/set_cluster_node_spec.py
@@ -10,9 +10,11 @@ cluster_node_ram = (cluster_node_ram + ram_overhead) * multipler_for_multiple_po
 print "CLUSTER_NODE_RAM=",cluster_node_ram
 
 if cluster_node_ram % 1024:
-    number_of_cores = (cluster_node_ram / 1024) + 1
+    number_of_cores = (cluster_node_ram / 1024)
+    print "number_of_cores=",number_of_cores
 else:
-    number_of_cores = cluster_node_ram / 1024
+    number_of_cores = (cluster_node_ram / 1024) + 1
+    print "number_of_cores=",number_of_cores
 # not possible to change the environment variables of parent processes!
 # os.environ['CLUSTER_NODE_CPUS'] = str(number_of_cores)
 # os.putenv('CLUSTER_NODE_CPUS', str(number_of_cores))
