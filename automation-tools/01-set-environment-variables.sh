a0eefebb7d2246e3c3fa0547552bb7082295a4a7
Terry Dhariwal <terry.dhariwal@me.com>
2018-09-21 12:43:49 +0100

    test automation tools 1 to 4

--- automation-tools/01-set-environment-variables.sh
+++ automation-tools/01-set-environment-variables.sh
@@ -9,7 +9,7 @@ export IMAGE_NAME=${CONTAINER_BASE_NAME}-java-${JAVA_VERSION}-tomcat-${TOMCAT_VE
 python ./set_cluster_node_spec.py | while read line ; do
     line=`echo $line | sed -re 's/ //g'`
     echo "$line"
-    eval `$line`
+    eval $line
 done
 echo "CLUSTER_NODE_RAM = $CLUSTER_NODE_RAM"
 echo "CLUSTER_NODE_CPUS = $CLUSTER_NODE_CPUS"
