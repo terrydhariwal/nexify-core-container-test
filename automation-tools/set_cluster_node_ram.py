4b5baadd8866208ff18f585f5260874f22b2273f
Terry Dhariwal <terry.dhariwal@me.com>
2018-09-21 11:52:28 +0100

    test automation tools 1 to 4

--- automation-tools/set_cluster_node_ram.py
+++ automation-tools/set_cluster_node_ram.py
@@ -6,4 +6,4 @@ multipler_for_multiple_pods=int(os.getenv('MULTIPLER_FOR_MULTIPLE_PODS', 1))
 cluster_node_ram=int(os.getenv('TOMCAT_RAM', 4096))
 cluster_node_ram+= (cluster_node_ram + ram_overhead) * multipler_for_multiple_pods
 cluster_node_ram
-return cluster_node_ram
+print cluster_node_ram
