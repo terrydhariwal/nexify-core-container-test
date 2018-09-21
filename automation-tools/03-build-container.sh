1247f3dc1761e8ce4955dca4834d1c984f41497b
Terry Dhariwal <terry.dhariwal@me.com>
2018-09-21 13:22:25 +0100

    test automation tools 1 to 4

--- automation-tools/03-build-container.sh
+++ automation-tools/03-build-container.sh
@@ -5,7 +5,7 @@ CURRENT_DIR=`pwd`
 echo ${CURRENT_DIR}
 cd ${CURRENT_DIR}/../lens2northwind/
 pwd
-docker build -t ${IMAGE_NAME} .
+gcloud builds submit --tag gcr.io/${PROJECT_ID}/quorum360:${IMAGE_NAME} .
 cd ${CURRENT_DIR}
-docker tag ${IMAGE_NAME} gcr.io/${PROJECT_ID}/quorum360:${IMAGE_NAME}
-docker push gcr.io/${PROJECT_ID}/quorum360:${IMAGE_NAME}
+# docker tag ${IMAGE_NAME} gcr.io/${PROJECT_ID}/quorum360:${IMAGE_NAME}
+# docker push gcr.io/${PROJECT_ID}/quorum360:${IMAGE_NAME}
