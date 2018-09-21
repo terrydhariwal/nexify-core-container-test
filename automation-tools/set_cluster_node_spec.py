a2cd33da87eab43777ffc064343ac9983c8705a9
Terry Dhariwal <terry.dhariwal@me.com>
2018-09-21 12:19:23 +0100

    test automation tools 1 to 4

--- automation-tools/set_cluster_node_spec.py
+++ automation-tools/set_cluster_node_spec.py
@@ -4,9 +4,13 @@ ram_overhead = int(os.getenv('RAM_OVERHEAD', 1024))
 multipler_for_multiple_pods = int(os.getenv('MULTIPLER_FOR_MULTIPLE_PODS', 1))
 cluster_node_ram= int(os.getenv('TOMCAT_RAM', 4096))
 cluster_node_ram = (cluster_node_ram + ram_overhead) * multipler_for_multiple_pods
+# not possible to change the environment variables of parent processes!
 # os.environ['CLUSTER_NODE_RAM'] = str(cluster_node_ram)
-os.putenv('CLUSTER_NODE_RAM', str(cluster_node_ram))
+# os.putenv('CLUSTER_NODE_RAM', str(cluster_node_ram))
+print cluster_node_ram
+
 number_of_cores = cluster_node_ram / 1024
-print "number_of_cores = ",number_of_cores
+# not possible to change the environment variables of parent processes!
 # os.environ['CLUSTER_NODE_CPUS'] = str(number_of_cores)
-os.putenv('CLUSTER_NODE_CPUS', str(number_of_cores))
+# os.putenv('CLUSTER_NODE_CPUS', str(number_of_cores))
+print number_of_cores
