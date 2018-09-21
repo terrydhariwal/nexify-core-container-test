49d55bd8261b55942c2486762f37d737355aa4a2
Terry Dhariwal <terry.dhariwal@me.com>
2018-09-21 22:09:57 +0100

    add CUSTOMER_NAME

--- automation-tools/02-set-gcloud.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-gcloud components update
-gcloud config set project ${PROJECT_ID};
-gcloud config set compute/zone ${COMPUTE_ZONE};
-gcloud beta auth configure-docker --quiet
--- container-build/tomcat/setenv.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#export CATALINA_OPTS="$CATALINA_OPTS -Xms12288m -Xmx12288m"
-export CATALINA_OPTS="$CATALINA_OPTS -Xms1024m -Xmx1024m"
-
-export CLASSPATH="$CLASSPATH:$CATALINA_HOME/lib/*:"`/opt/hbase/bin/hbase classpath`
