a4a43be3564ba3739bb40da2989e8acf7ac99d59
Terry Dhariwal <terry.dhariwal@me.com>
2018-09-21 22:24:30 +0100

    fix bug

--- build-all.sh
+++ build-all.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+CURRENT_DIR=`pwd`
 SCRIPTS_DIRECTORY=container-automation
 
-source ./${SCRIPTS_DIRECTORY}/01-set-environment-variables.sh
-source ./${SCRIPTS_DIRECTORY}/02-set-gcloud.sh
-source ./${SCRIPTS_DIRECTORY}/03-build-container.sh
-source ./${SCRIPTS_DIRECTORY}/04-create-cluster-if-not-exists.sh
-source ./${SCRIPTS_DIRECTORY}/05-deploy-container-image.sh
-source ./${SCRIPTS_DIRECTORY}/06-test-deployment.sh
+cd ${SCRIPTS_DIRECTORY}
+source ./01-set-environment-variables.sh
+source ./02-set-gcloud.sh
+source ./03-build-container.sh
+source ./04-create-cluster-if-not-exists.sh
+source ./05-deploy-container-image.sh
+source ./06-test-deployment.sh
+cd ${CURRENT_DIR}
--- teardown-all.sh
+++ teardown-all.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+CURRENT_DIR=`pwd`
 SCRIPTS_DIRECTORY=container-automation
 
-source ./${SCRIPTS_DIRECTORY}/01-set-environment-variables.sh
-source ./${SCRIPTS_DIRECTORY}/02-set-gcloud.sh
-source ./${SCRIPTS_DIRECTORY}/07-delete-deployment.sh
-source ./${SCRIPTS_DIRECTORY}/08-tear-down-cluster.sh
+cd ${SCRIPTS_DIRECTORY}
+source ./01-set-environment-variables.sh
+source ./02-set-gcloud.sh
+source ./07-delete-deployment.sh
+source ./08-tear-down-cluster.sh
+cd ${CURRENT_DIR}
