c8f8acb3d51ba35df782be5ea6a67c5171af565f
Terry Dhariwal <terry.dhariwal@me.com>
2018-09-21 11:37:41 +0100

    test automation tools 1 to 4

--- automation-tools/set_cluster_node_ram.py
+++ automation-tools/set_cluster_node_ram.py
@@ -1,15 +1,9 @@
 import os
 
-
-# using get will return `None` if a key is not present rather than raise a `KeyError`
-#print(os.environ.get('tomcat_ram'))
-
-# os.getenv is equivalent, and can also give a default value instead of `None`
-# default_value=4096
-# print(os.getenv('tomcat_ram', default_value))
-
-# at minimum give cluster node 20% overhead over tomcat ram
-percent_to_raise=int(os.getenv('percent_to_raise', 20))
+# at minimum increase ram by 1024
+ram_overhead=int(os.getenv('ram_overhead', 1024))
+multipler_for_multiple_pods=int(os.getenv('multipler_for_multiple_pods', 1))
 cluster_node_ram=int(os.getenv('tomcat_ram', 4096))
-cluster_node_ram+= cluster_node_ram / 100 * 20
-print cluster_node_ram
+cluster_node_ram+= (cluster_node_ram + ram_overhead) * multipler_for_multiple_pods
+cluster_node_ram
+return cluster_node_ram
