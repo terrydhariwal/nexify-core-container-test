a3bb7fc9a15d299c8522eb0a120a0bc4afced2ec
Terry Dhariwal <terry.dhariwal@me.com>
2018-09-21 11:46:26 +0100

    test automation tools 1 to 4

--- automation-tools/03-build-container.sh
+++ automation-tools/03-build-container.sh
@@ -1,9 +1,11 @@
-#CURRENT_DIR=$(dirname $0)
+#need to call scripts using source - since this works to set environment variables
+#CURRENT_DIR=$(dirname $0) # does not work with source ./this-script
+# when using source we need to use pwd
 CURRENT_DIR=`pwd`
 echo ${CURRENT_DIR}
 cd ${CURRENT_DIR}/../lens2northwind/
 pwd
-docker build -t ${image_name} .
+docker build -t ${IMAGE_NAME} .
 cd ${CURRENT_DIR}
-docker tag ${image_name} gcr.io/${PROJECT_ID}/quorum360:${image_name}
-docker push gcr.io/${PROJECT_ID}/quorum360:${image_name}
+docker tag ${IMAGE_NAME} gcr.io/${PROJECT_ID}/quorum360:${IMAGE_NAME}
+docker push gcr.io/${PROJECT_ID}/quorum360:${IMAGE_NAME}
