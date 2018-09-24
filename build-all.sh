029a3c805e6aaf946836132b468f971381f81672
Terry Dhariwal <terry.dhariwal@me.com>
2018-09-24 13:13:38 +0100

    debug

--- build-all.sh
+++ build-all.sh
@@ -10,7 +10,7 @@ echo "3 = $3"
 echo "4 = $4"
 
 cd ${SCRIPTS_DIRECTORY}
-#source ./01-set-environment-variables.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20} ${21} ${22} ${23} ${24} ${25} ${26} ${27} ${28}
+source ./01-set-environment-variables.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20} ${21} ${22} ${23} ${24} ${25} ${26} ${27} ${28}
 #source ./02-set-gcloud.sh
 # source ./03-build-container.sh
 # source ./04-create-cluster-if-not-exists.sh
