d3321cb5e979c1059cfe7977f60bb6bd3ef43b42
Terry Dhariwal <terry.dhariwal@me.com>
2018-09-24 11:04:18 +0100

    fixing bug

--- test-env-subshell.sh
+++ test-env-subshell.sh
@@ -5,14 +5,6 @@
 #  echo "source ./test-env-subshell.sh --customer-name test123"
 # }
 
-CURRENT_DIR=`pwd`
-SCRIPTS_DIRECTORY=container-automation
-cd ${SCRIPTS_DIRECTORY}
-source ./01-set-environment-variables.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20} ${21} ${22} ${23} ${24} ${25} ${26} ${27} ${28}
-cd ${CURRENT_DIR}
-echo "CUSTOMER_NAME=${CUSTOMER_NAME}"
-echo "HALYARD_VERSION=${HALYARD_VERSION}"
-
 # while [ "$1" != "" ]; do
 #     case $1 in
 #         --customer-name )          shift
@@ -30,4 +22,10 @@ echo "HALYARD_VERSION=${HALYARD_VERSION}"
 #     shift
 # done
 
-# run-01-set-environment-variables
+CURRENT_DIR=`pwd`
+SCRIPTS_DIRECTORY=container-automation
+cd ${SCRIPTS_DIRECTORY}
+source ./01-set-environment-variables.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20} ${21} ${22} ${23} ${24} ${25} ${26} ${27} ${28}
+cd ${CURRENT_DIR}
+echo "CUSTOMER_NAME=${CUSTOMER_NAME}"
+echo "HALYARD_VERSION=${HALYARD_VERSION}"
