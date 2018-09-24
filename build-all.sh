c37c1c5dbf55c5b9f864593f27bdd34c9b206c69
Terry Dhariwal <terry.dhariwal@me.com>
2018-09-24 13:15:53 +0100

    debug

--- build-all.sh
+++ build-all.sh
@@ -3,17 +3,17 @@
 CURRENT_DIR=`pwd`
 SCRIPTS_DIRECTORY=container-automation
 
-echo "in build-all"
-echo "1 = $1"
-echo "2 = $2"
-echo "3 = $3"
-echo "4 = $4"
+# echo "in build-all"
+# echo "1 = $1"
+# echo "2 = $2"
+# echo "3 = $3"
+# echo "4 = $4"
 
 cd ${SCRIPTS_DIRECTORY}
 source ./01-set-environment-variables.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20} ${21} ${22} ${23} ${24} ${25} ${26} ${27} ${28}
-#source ./02-set-gcloud.sh
-# source ./03-build-container.sh
-# source ./04-create-cluster-if-not-exists.sh
-# source ./05-deploy-container-image.sh
-# source ./06-test-deployment.sh
+source ./02-set-gcloud.sh
+source ./03-build-container.sh
+source ./04-create-cluster-if-not-exists.sh
+source ./05-deploy-container-image.sh
+source ./06-test-deployment.sh
 cd ${CURRENT_DIR}
